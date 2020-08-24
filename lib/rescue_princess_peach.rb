05dcdf7eebbacd689e9b9f81015f0afd37648a94
binding-proc-8938 <naomirhames2020@gmail.com>
2020-08-24 02:29:04 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,6 @@
 def rescue_princess_peach
   puts status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
+  puts "Hooray! Mario has rescued Princess Peach." e
 end
 
 ## Comment the below back in to see method scope in action!
